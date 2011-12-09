fe1c3ca6b804fdf7734b1010127c37ac34a8efbb
Matt Snyder <matt@cranialpulse.com>
2011-12-09 16:32:58 -0500

    Add bindings for accessing rinari functions, splitting windows and additional modes

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
@@ -5,10 +8,36 @@
              '("tromey" . "http://tromey.com/elpa/") t)
 (package-initialize)
 
+;; Color-Theme
+;;(add-to-list 'load-path "elpa/color-theme-6.6.1/color-theme.el")
+(require 'color-theme)
+;;(color-theme-initialize)
+
 ;; Ruby
  (autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
    (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
    (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+   (add-to-list 'auto-mode-alist '("\\.js\.erb\\'" . ruby-mode))
+
+;; SASS
+(require 'sass-mode)
+(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
+
+;; HAML
+(require 'haml-mode)
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(define-key haml-mode-map [(control meta down)] 'haml-forward-sexp)
+(define-key haml-mode-map [(control meta up)] 'haml-backward-sexp)
+(define-key haml-mode-map [(control meta left)] 'haml-up-list)
+(define-key haml-mode-map [(control meta right)] 'haml-down-list)
+
+;; Line numbers for Ruby
+(add-hook 'abg-code-modes-hook
+          (lambda () (linum-mode 1)))
+
+(add-hook 'ruby-mode-hook
+          (lambda () (run-hooks 'abg-code-modes-hook)))
+
 
 ;; Javascript
 (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
@@ -23,8 +52,17 @@
 (ido-mode t)
 
 ;; rinari 
-(add-to-list 'load-path "~/.emacs.d/rinari") 
 (require 'rinari)
+(define-key rinari-minor-mode-map [(control meta shift down)] 'rinari-find-rspec)
+(define-key rinari-minor-mode-map [(control meta shift up)] 'rinari-find-controller)
+(define-key rinari-minor-mode-map [(control meta shift left)] 'rinari-find-model)
+(define-key rinari-minor-mode-map [(control meta shift right)] 'rinari-find-view)
+
+;; Snippets
+;;(add-to-list 'load-path (concat dotfiles-dir "/vendor/yasnippet.el"))
+(require 'yasnippet)
+;;(yas/initialize)
+;;(yas/load-directory (concat dotfiles-dir "/vendor/yasnippet.el/snippets"))
 
 ;; Flymake Ruby
 (require 'flymake)
@@ -54,3 +92,67 @@
 	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
 		 (flymake-mode))
 	     ))
+
+
+;; Full screen toggle
+(defun toggle-fullscreen()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+(global-set-key (kbd "M-n") 'toggle-fullscreen)
+
+;; Split Windows
+(global-set-key [f6] 'split-window-horizontally)
+(global-set-key [f7] 'split-window-vertically)
+(global-set-key [f8] 'delete-window)
+
+;; Some Mac-friendly key counterparts
+(global-set-key (kbd "M-s") 'save-buffer)
+(global-set-key (kbd "M-z") 'undo)
+(global-set-key (kbd "M-c") 'copy-region-as-kill)
+(global-set-key (kbd "M-a") 'mark-whole-buffer)
+
+;; Keyboard Overrides
+;;(define-key textile-mode-map (kbd "M-s") 'save-buffer)
+;;(define-key text-mode-map (kbd "M-s") 'save-buffer)
+
+(global-set-key [(meta up)] 'beginning-of-buffer)
+(global-set-key [(meta down)] 'end-of-buffer)
+
+(global-set-key [(meta shift right)] 'ido-switch-buffer)
+(global-set-key [(meta shift up)] 'recentf-ido-find-file)
+(global-set-key [(meta shift down)] 'ido-find-file)
+(global-set-key [(meta shift left)] 'magit-status)
+
+(global-set-key [(meta H)] 'delete-other-windows)
+(global-set-key [(meta D)] 'backward-kill-word)
+(global-set-key [(meta N)] 'cleanup-buffer)
+(global-set-key [(control \])] 'indent-rigidly)
+
+(prefer-coding-system 'utf-8)
+
+;; Allow EM and EN dashes
+;; (require 'typopunct)
+;; (typopunct-change-language 'english t)
+
+;; eshell preferences
+(add-hook 'eshell-mode-hook
+          '(lambda nil
+             (let ((path))
+               (setq path "~/bin:/opt/local/bin:/usr/local/bin:/usr/bin:/bin")
+               (setenv "PATH" path))
+             (local-set-key "\C-u" 'eshell-kill-input)))
+
+(defun eshell/cl ()
+  "Command to clear the eshell buffer."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)))
+
+;; Org-mode settings
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+
+
